40782a7cd819047d70e8b5121d763a118206c643
Mohotazim Hossen Bosunia <zimhossen0@gmail.com>
2026-04-25 11:59:30 +0600

    sixth commit

--- src/Components/PricingOptions/PricingOption.jsx
+++ src/Components/PricingOptions/PricingOption.jsx
@@ -5,7 +5,7 @@ const PricingOption = ({ priceData }) => {
   //   console.log(priceData);
   const { name, price, duration, features } = priceData;
   return (
-    <div className="border bg-fuchsia-300 rounded-2xl mt-4 p-2.5">
+    <div className="flex flex-col border bg-fuchsia-300 rounded-2xl mt-4 p-2.5">
       <div className="ml-4">
         <h1 className="text-5xl">{name}</h1>
         <h2 className="text-2xl">
@@ -13,11 +13,14 @@ const PricingOption = ({ priceData }) => {
           {duration}
         </h2>
       </div>
-      <div className=" border bg-amber-200 rounded-2xl m-4">
+      <div className="flex-1 border bg-amber-200 rounded-2xl m-4">
         {features.map((option, index) => (
           <Feature key={index} option={option}></Feature>
         ))}
       </div>
+      <button className="w-full bg-blue-400 border rounded-md p-2 hover:bg-emerald-400 ">
+        Subscribe
+      </button>
     </div>
   );
 };
